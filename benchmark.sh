b4659942428e4be8b2c6011efcb7fd20c4e6a817
oswaldlo1 <oswaldl@student.ethz.ch>
2022-05-12 15:51:45 +0200

    Initial shell script

---
+++ benchmark.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+BENCHMARK_DIR=$(pwd)
+MEMPOOL_DIR=/scratch/bsc22f8/git/oswaldlo1/mempool
+SNITCH_DIR=/scratch/bsc22f8/git/oswaldlo1/snitch
+BANSHEE_DIR=$SNITCH_DIR/sw/banshee
+
+### Setup ###
+# clean files
+rm -f {multicore,singlecore}/{apps,banshee-results,rtl-results}/{baseline,xpulp}/*
+
+# clean mempool
+cd $MEMPOOL_DIR/software
+make clean
+cd ..
+
+# exit
+
+### Generate test apps ###
+echo "Generate test apps"
+# without xpulp enabled
+config=minpool make XPULPIMG=0 apps
+# copy to save location
+cp -rf software/bin/* $BENCHMARK_DIR/multicore/apps/baseline
+# with xpulp enabled
+config=minpool make XPULPIMG=1 apps
+# copy to save location
+cp -rf software/bin/* $BENCHMARK_DIR/multicore/apps/xpulp
+
+# exit
+
+### Run benchmarks ###
+echo "Run benchmarks"
+cd $MEMPOOL_DIR/hardware
+unset CC && unset CXX
+# make clean #&& config=minpool make simcvcs
+
+# without xpulp enabled
+cp -rf $BENCHMARK_DIR/multicore/apps/baseline/* $MEMPOOL_DIR/software/bin
+app=convolution config=minpool make benchmark | tee $BENCHMARK_DIR/multicore/rtl-results/baseline/convolution
+
+cd $BANSHEE_DIR
+SNITCH_LOG=banshee::engine=TRACE cargo run -- --num-cores 16 --num-clusters 1 --configuration config/mempool.yaml \
+    $MEMPOOL_DIR/software/bin/convolution --latency &> $BENCHMARK_DIR/multicore/banshee-results/baseline/convolution
+
+# exit
+
+# with xpulp enabled
+cp -rf $BENCHMARK_DIR/multicore/apps/xpulp/* $MEMPOOL_DIR/software/bin
+app=convolution config=minpool make benchmark | tee $BENCHMARK_DIR/multicore/rtl-results/xpulp/convolution
+
+cd $BANSHEE_DIR
+SNITCH_LOG=banshee::engine=TRACE cargo run -- --num-cores 16 --num-clusters 1 --configuration config/mempool.yaml \
+    $MEMPOOL_DIR/software/bin/convolution --latency &> $BENCHMARK_DIR/multicore/banshee-results/xpulp/convolution
+
